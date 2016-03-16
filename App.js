5d30e36b531f39e71d2514691c579996ff4116a3
Homan Chou <homanchou@gmail.com>
2016-03-16 00:43:57 -0700

    lesson 7, extra credit color picker

--- App.js
+++ App.js
@@ -10,19 +10,21 @@ class App extends React.Component {
   update(e){
     this.setState({
       red: ReactDOM.findDOMNode(this.refs.red).value,
-      blue: ReactDOM.findDOMNode(this.refs.blue).value,
-      green: ReactDOM.findDOMNode(this.refs.green).value
+      green: ReactDOM.findDOMNode(this.refs.green).value,
+      blue: ReactDOM.findDOMNode(this.refs.blue).value
     })
   }
   render(){
     return (
       <div>
+        <div style={{border:'1px solid black', width:'100px', height:'100px', backgroundColor:'rgb('+this.state.red+','+this.state.green+','+this.state.blue+')'}}>
+        </div>
         <Slider ref="red" value={this.state.red} update={this.update}/>
         {this.state.red}<br/>
-        <Slider ref="blue" value={this.state.blue} update={this.update}/>
-        {this.state.blue}<br/>
         <Slider ref="green" value={this.state.green} update={this.update}/>
         {this.state.green}<br/>
+        <Slider ref="blue" value={this.state.blue} update={this.update}/>
+        {this.state.blue}<br/>
       </div>
     )
   }
