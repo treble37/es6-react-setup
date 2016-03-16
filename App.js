850fe3de1a5a45b16687cca00231d27921e897b3
Homan Chou <homanchou@gmail.com>
2016-03-15 23:51:50 -0700

    lesson 5, initial state

--- App.js
+++ App.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {txt: 'this is the state text'}
+  }
   render(){
-    let txt = this.props.txt
-    return <h1>{txt}</h1>
+    return <h1>{this.state.txt}</h1>
   }
 }
 
